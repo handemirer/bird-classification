53fb7eaf9ae4e926fd6d94d680ad3139494fb98a
Berkay Özdemir <berkay.ozdemir@optiyol.com>
2022-05-15 15:36:47 +0300

    some updates

--- birdclassification/lib/home.dart
+++ birdclassification/lib/home.dart
@@ -1,14 +1,11 @@
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:birdclassification/bc_navigator_push.dart';
-import 'package:birdclassification/gallery.dart';
 import 'package:birdclassification/homepage.dart';
 import 'package:birdclassification/screens/ImagePredict.dart';
 import 'package:birdclassification/screens/LiveCamera.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'models.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -33,14 +30,15 @@ class _HomePageState extends State<HomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset("assets/images/bird.jpeg"),
+              padding: const EdgeInsets.all(15.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset("assets/images/bird.jpeg")),
             ),
             ElevatedButton(
-                child: const Text("LİVE"),
+                child: const Text("Kamerayla Kuş Keşfet"),
                 onPressed: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
@@ -48,18 +46,18 @@ class _HomePageState extends State<HomePage> {
                     ),
                   );
                 }),
-            ElevatedButton(
-              child: const Text("Gallery"),
-              onPressed: () {
-                //showMediaInputs();
-                //bcNavigatorPush(context: context, page: const Gallery());
-              },
-            ),
-            ElevatedButton(
-              child: const Text("Gallery"),
-              onPressed: () =>
-                  bcNavigatorPush(context: context, page: HomePage2()),
-            ),
+            // ElevatedButton(
+            //   child: const Text("Gallery"),
+            //   onPressed: () {
+            //     //showMediaInputs();
+            //     //bcNavigatorPush(context: context, page: const Gallery());
+            //   },
+            // ),
+            // ElevatedButton(
+            //   child: const Text("Gallery"),
+            //   onPressed: () =>
+            //       bcNavigatorPush(context: context, page: HomePage2()),
+            // ),
           ],
         ),
       ),
@@ -70,53 +68,75 @@ class _HomePageState extends State<HomePage> {
     showModalBottomSheet(
         context: context,
         builder: (context) {
-          return Container(
-            margin: const EdgeInsets.all(8),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          return DraggableScrollableSheet(
+            expand: false,
+            initialChildSize: 0.3,
+            builder: (context, scrollController) {
+              return Container(
+                margin: const EdgeInsets.all(8),
+                child: Column(
                   children: [
-                    FloatingActionButton.extended(
-                      onPressed: (() async {
-                        final XFile? image = await ImagePicker()
-                            .pickImage(source: ImageSource.gallery);
-                        if (image != null) {
-                          //imageFile = File(image.path);
-                          Navigator.of(context)
-                              .pushReplacement(MaterialPageRoute(
-                            builder: (context) => ImagePredict(
-                              imageFile: File(image.path),
-                            ),
-                          ));
-                        }
-                      }),
-                      elevation: 0,
-                      label: Text("Galeriden Seç"),
-                      icon: Icon(Icons.camera_alt_rounded),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 11),
+                      width: 60,
+                      height: 6,
+                      decoration: BoxDecoration(
+                          color: Colors.white60,
+                          borderRadius: BorderRadius.circular(100)),
                     ),
-                    FloatingActionButton.extended(
-                      onPressed: (() async {
-                        final XFile? image = await ImagePicker()
-                            .pickImage(source: ImageSource.camera);
-                        if (image != null) {
-                          //imageFile = File(image.path);
-                          Navigator.of(context)
-                              .pushReplacement(MaterialPageRoute(
-                            builder: (context) => ImagePredict(
-                              imageFile: File(image.path),
-                            ),
-                          ));
-                        }
-                      }),
-                      elevation: 0,
-                      label: Text("Fotoğraf Çek"),
-                      icon: Icon(Icons.camera_alt_rounded),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 15),
+                      child: Text(
+                        "Kuş Türü Bul",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        FloatingActionButton.extended(
+                          onPressed: (() async {
+                            final XFile? image = await ImagePicker()
+                                .pickImage(source: ImageSource.gallery);
+                            if (image != null) {
+                              //imageFile = File(image.path);
+                              Navigator.of(context)
+                                  .pushReplacement(MaterialPageRoute(
+                                builder: (context) => ImagePredict(
+                                  imageFile: File(image.path),
+                                ),
+                              ));
+                            }
+                          }),
+                          elevation: 0,
+                          label: Text("Galeriden Seç"),
+                          icon: Icon(Icons.camera_alt_rounded),
+                        ),
+                        FloatingActionButton.extended(
+                          onPressed: (() async {
+                            final XFile? image = await ImagePicker()
+                                .pickImage(source: ImageSource.camera);
+                            if (image != null) {
+                              //imageFile = File(image.path);
+                              Navigator.of(context)
+                                  .pushReplacement(MaterialPageRoute(
+                                builder: (context) => ImagePredict(
+                                  imageFile: File(image.path),
+                                ),
+                              ));
+                            }
+                          }),
+                          elevation: 0,
+                          label: Text("Fotoğraf Çek"),
+                          icon: Icon(Icons.camera_alt_rounded),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              );
+            },
           );
         });
   }
