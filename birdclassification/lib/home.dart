ff3f8d72a9b9e2cae44a0ab39a5c2d4606129000
Han Demirer <43323219+handemirer@users.noreply.github.com>
2022-05-15 19:16:57 +0300

    Update home.dart

--- birdclassification/lib/home.dart
+++ birdclassification/lib/home.dart
@@ -111,7 +111,7 @@ class _HomePageState extends State<HomePage> {
                           }),
                           elevation: 0,
                           label: Text("Galeriden Seç"),
-                          icon: Icon(Icons.camera_alt_rounded),
+                          icon: Icon(Icons.image),
                         ),
                         FloatingActionButton.extended(
                           onPressed: (() async {
